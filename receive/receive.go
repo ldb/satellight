53bbe71f5f0c0c62b7e00c8f2eacd2f63c7177ab
Leo <10501668+ldb@users.noreply.github.com>
2022-07-12 17:32:55 +0200

    receiver
    Co-authored-by: isabel <isaschm@users.noreply.github.com>

--- receive/receive.go
+++ receive/receive.go
@@ -1,9 +1,49 @@
 package receive
 
-import "net/http"
+import (
+	"github.com/ldb/satellight/protocol"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 const defaultListenAddress = ":8000"
 
+type SpaceMessageHandler func(message protocol.SpaceMessage)
+
 type Receiver struct {
 	server http.Server
+}
+
+func NewReceiver(addr string, handle SpaceMessageHandler) *Receiver {
+	r := Receiver{
+		server: http.Server{
+			Addr:    addr,
+			Handler: handleMessage(handle),
+		},
+	}
+	return &r
+}
+
+func handleMessage(msgHandle SpaceMessageHandler) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		buf, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			log.Printf("error reading request: %v", err)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		msg := new(protocol.SpaceMessage)
+		if err := msg.UnmarshalSpaceMessage(buf); err != nil {
+			log.Printf("error unmarshaling SpaceMessage: %v", err)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		msgHandle(*msg)
+		writer.WriteHeader(http.StatusOK)
+	}
+}
+
+func (r *Receiver) Run() error {
+	return r.server.ListenAndServe()
 }
